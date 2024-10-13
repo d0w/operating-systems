54526716fae84cac458bee5290bbd2a3489acac2
Derek <derekxu888@gmail.com>
2024-10-13 18:46:47 -0400

    start framework of threading library

--- hw2/ec440threads.h
+++ hw2/ec440threads.h
@@ -1,46 +1,133 @@
 #ifndef __EC440THREADS__
 #define __EC440THREADS__
 
-unsigned long int ptr_demangle(unsigned long int p)
-{
-    unsigned long int ret;
+#include <pthread.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
 
-    asm("movq %1, %%rax;\n"
-        "rorq $0x11, %%rax;"
-        "xorq %%fs:0x30, %%rax;"
-        "movq %%rax, %0;"
-    : "=r"(ret)
-    : "r"(p)
-    : "%rax"
-    );
-    return ret;
-}
+#define JB_RBX 0
+#define JB_RBP 1
+#define JB_R12 2
+#define JB_R13 3
+#define JB_R14 4
+#define JB_R15 5
+#define JB_RSP 6
+#define JB_PC 7
 
-unsigned long int ptr_mangle(unsigned long int p)
-{
-    unsigned long int ret;
 
-    asm("movq %1, %%rax;\n"
-        "xorq %%fs:0x30, %%rax;"
-        "rolq $0x11, %%rax;"
-        "movq %%rax, %0;"
-    : "=r"(ret)
-    : "r"(p)
-    : "%rax"
-    );
-    return ret;
-}
+#define MAX_THREADS 128
+#define STACK_SIZE 32767
+#define QUANTUM 50000 // 50000 microseconds = 50 milliseconds
 
-void *start_thunk() {
-  asm("popq %%rbp;\n"           //clean up the function prolog
-      "movq %%r13, %%rdi;\n"    //put arg in $rdi
-      "pushq %%r12;\n"          //push &start_routine
-      "retq;\n"                 //return to &start_routine
-      :
-      :
-      : "%rdi"
-  );
-  __builtin_unreachable();
-}
+
+enum threadStatus {
+    READY,
+    RUNNING,
+    BLOCKED,
+    EXITED
+};
+
+struct TCB {
+    pthread_t tid;
+    jmp_buf context;
+    enum threadStatus status;
+    
+    // allocated stack
+    void *stack;
+};
+
+// ------------------ LIBRARY -----------------
+/**
+ * Sets the register value in the context
+ * @param context Context to set the register in
+ * @param reg Register to set
+ * @param value Value to set the register to
+ * @return Index in jmp_buf as defined by the OS if successful, -1 if error
+ */
+int setRegister(jmp_buf *context, int reg, unsigned long int value);
+
+/**
+ * Gets the register value in the context
+ * @param context Context to get the register from
+ * @param reg Register to get
+ * @return Value of the register if successful, -1 if error
+ */
+unsigned long int getRegister(jmp_buf *context, int reg);
+
+/**
+ * Schedules the next thread to run
+ * @return void
+ */
+void schedule();
+
+/**
+ * Run on first thread creation to initialize threading. 
+ * Sets up the main thread and timer for the scheduler.
+ */
+void initializeThreading();
+
+/**
+ * Creates a thread. Sets up TCB for the scheduler to choose from.
+ * @param thread Pointer to the thread
+ * @param attr Attributes of the thread - always null
+ * @param start_routine Function to run in the thread
+ * @param arg Argument to pass to the function
+ * @return exit status of the thread or -1 for errors
+ */
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
+
+
+/**
+ * Terminates calling thread. The process shall exit with an exit status of 0 after the last
+thread has been terminated. The behavior shall be as if the implementation called exit() with a
+zero argument at thread termination time.
+ * @param value_ptr Exit status of the thread - ignored here
+ * @return void
+ */
+void pthread_exit(void *value_ptr);
+
+/**
+ * Returns the thread ID of the calling thread.
+ * @return Thread ID of the calling thread
+ */
+pthread_t pthread_self(void);
+
+
+
+
+// JMP_BUF DEFINITION taken from bandit
+/* Define the machine-dependent type `jmp_buf'.  x86-64 version.  */
+// #ifndef _BITS_SETJMP_H
+// #define _BITS_SETJMP_H  1
+
+// #if !defined _SETJMP_H && !defined _PTHREAD_H
+// # error "Never include <bits/setjmp.h> directly; use <setjmp.h> instead."
+// #endif
+
+// #include <bits/wordsize.h>
+
+// #ifndef _ASM
+
+// # if __WORDSIZE == 64
+// typedef long int __jmp_buf[8];
+// # elif defined  __x86_64__
+// __extension__ typedef long long int __jmp_buf[8];
+// # else
+// typedef int __jmp_buf[6];
+// # endif
+
+// #endif
+
+// #endif  /* bits/setjmp.h */
+
+unsigned long int ptr_demangle(unsigned long int p);
+
+unsigned long int ptr_mangle(unsigned long int p);
+
+void *start_thunk();
 
 #endif
---
+++ hw2/threads.c
@@ -0,0 +1,241 @@
+#include "ec440threads.h"
+
+// function documentation in header file
+
+// ------------------ GLOBAL VARIABLES ------------------
+struct TCB *gThreads[MAX_THREADS] = { NULL };
+pthread_t gCurrent = 0;
+
+
+// ------------------ FUNCTIONS -------------------------
+
+// func documentation in header file
+int setRegister(jmp_buf *context, int reg, unsigned long int value) {
+    // set registers
+    switch(reg) {
+        // all registers that need to be mangled
+        case JB_PC:
+        case JB_RSP:
+        case JB_RBP:
+        case JB_RBX:
+            (*context)->__jmpbuf[reg] = ptr_mangle(value);
+            return reg;
+
+        // all registers that do not need to be mangled
+        case JB_R12:
+        case JB_R13:
+        case JB_R14:
+        case JB_R15:
+            (*context)->__jmpbuf[reg] = value;
+            return reg;
+    }
+    return -1;
+}
+
+
+unsigned long int getRegister(jmp_buf *context, int reg) {
+    // get registers
+    switch(reg) {
+        // all registers that need to be demangled
+        case JB_PC:
+        case JB_RSP:
+        case JB_RBP:
+        case JB_RBX:
+            return ptr_demangle((*context)->__jmpbuf[reg]);
+
+        // all registers that do not need to be demangled
+        case JB_R12:
+        case JB_R13:
+        case JB_R14:
+        case JB_R15:
+            return (*context)->__jmpbuf[reg];
+    }
+    return -1;
+}
+
+
+void schedule() {
+    // save current thread
+    printf("Switching from thread %ld\n", gCurrent);
+    if (gThreads[gCurrent] != NULL) {
+        if (setjmp(gThreads[gCurrent]->context) == 0) {
+            // set status to ready
+            gThreads[gCurrent]->status = READY;
+
+        } else {
+            perror("Error saving thread");
+        }
+    }
+
+    // find next thread to run using round robin
+    int next = (gCurrent + 1) % MAX_THREADS;
+    // keep looping until we find a thread that is ready
+    while (gThreads[next] == NULL || gThreads[next]->status != READY) {
+        next = (next + 1) % MAX_THREADS;
+    }
+
+    // set status to running and update gCurrent
+    gThreads[next]->status = RUNNING;
+    gCurrent = next;
+
+    // run next thread
+    longjmp(gThreads[next]->context, 1); 
+}
+
+
+void initializeThreading() {
+    // set up main thread
+    struct TCB *mainThread = malloc(sizeof(struct TCB));
+    mainThread->stack = NULL;
+    mainThread->tid = 0;
+    mainThread->status = RUNNING;
+    gThreads[0] = mainThread;
+    gCurrent = 0;
+
+    // set up timer using timer and signal handler
+    // configure timer
+    struct itimerval timer;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = QUANTUM;
+    timer.it_value = timer.it_interval;
+
+    // signal handler
+    struct sigaction sa;
+    sa.sa_handler = &schedule;
+    sa.sa_flags = SA_NODEFER;
+    sigaction(SIGALRM, &sa, NULL);
+
+
+    // start timer
+    if (setitimer(ITIMER_REAL, &timer, NULL) < 0) {
+        perror("Error setting timer");
+        exit(1);
+    }
+
+}
+
+
+
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg) {
+    static int initialized = 0;
+    if (!initialized) {
+        // initialize program to start multithreading (scheduler and create TCB for main)
+        // perform initializtion here
+        initializeThreading();
+        initialized = 1;
+    }
+
+    // find empty slot in gThreads
+    int id = 0;
+    for (id = 0; id < MAX_THREADS; id++) {
+        if (id >= MAX_THREADS) {
+            perror("Max threads reached");
+            return -1;
+        }
+        else if (gThreads[id] == NULL) {
+            break;
+        }
+    }
+
+    // create TCB
+    struct TCB *newThread = malloc(sizeof(struct TCB));
+
+    // create stack, set stack pointer in TCB
+    newThread->stack = malloc(STACK_SIZE);
+
+    // set id of thread and add to thread list
+    gThreads[id] = newThread;
+    *thread = id;
+    newThread->tid = id;
+
+
+    // since an implicit call to pthreadexit is required, we need to set a ptr 
+    // to the function the bottom of the stack (notation following will assume the stack grows down)
+    // want to place address of pthread_exit at the top of the stack
+    
+    // find stack top (growing down) and go down by one function pointer
+    void *stackTop = newThread->stack + STACK_SIZE;
+    void *stackPtr = stackTop - sizeof(void *);
+    
+    // get pointer to pthread exit
+    // copy pthread exit pointer into the stack so it is called when start_routine is done
+    memcpy(stackPtr, (void *)(&pthread_exit), sizeof(void *));
+
+
+    // set context into jmp_buf
+    // set PC to start_thunk to insert args. Function Places R13 value into the first argument to function in R12
+    // also need to set RSP to the stack pointer
+    setRegister(&(newThread->context), JB_PC, (unsigned long int)start_thunk);
+    setRegister(&(newThread->context), JB_R12, (unsigned long int)arg);
+    setRegister(&(newThread->context), JB_R13, (unsigned long int)start_routine);
+    setRegister(&(newThread->context), JB_RSP, (unsigned long int)(stackPtr));
+
+    // set status to ready
+    newThread->status = READY;
+
+    return 0;
+}
+
+void pthread_exit(void *value_ptr) {
+    // set status
+    gThreads[gCurrent]->status = EXITED;
+
+    // free values in TCB
+    free(gThreads[gCurrent]->stack);
+    free(gThreads[gCurrent]);
+    gThreads[gCurrent] = NULL;
+
+    if (gThreads[gCurrent] != NULL) {
+        fprintf(stderr, "Error freeing thread: %ld", gCurrent);
+    }
+
+    exit(0);
+}
+
+
+pthread_t pthread_self(void) {
+    return gCurrent;
+}
+
+unsigned long int ptr_demangle(unsigned long int p)
+{
+    unsigned long int ret;
+
+    asm("movq %1, %%rax;\n"
+        "rorq $0x11, %%rax;"
+        "xorq %%fs:0x30, %%rax;"
+        "movq %%rax, %0;"
+    : "=r"(ret)
+    : "r"(p)
+    : "%rax"
+    );
+    return ret;
+}
+
+unsigned long int ptr_mangle(unsigned long int p)
+{
+    unsigned long int ret;
+
+    asm("movq %1, %%rax;\n"
+        "xorq %%fs:0x30, %%rax;"
+        "rolq $0x11, %%rax;"
+        "movq %%rax, %0;"
+    : "=r"(ret)
+    : "r"(p)
+    : "%rax"
+    );
+    return ret;
+}
+
+void *start_thunk() {
+  asm("popq %%rbp;\n"           //clean up the function prolog
+      "movq %%r13, %%rdi;\n"    //put arg in $rdi
+      "pushq %%r12;\n"          //push &start_routine
+      "retq;\n"                 //return to &start_routine
+      :
+      :
+      : "%rdi"
+  );
+  __builtin_unreachable();
+}
